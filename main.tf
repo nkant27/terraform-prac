cfb0670689cceba5ee2fe40aabda842255069a55
nishi kant <nkant27@gmail.com>
2026-09-17 21:47:07 +0530

    add main.tf file

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg1-resourcegroup"
+  location = "West Europe"
+}
